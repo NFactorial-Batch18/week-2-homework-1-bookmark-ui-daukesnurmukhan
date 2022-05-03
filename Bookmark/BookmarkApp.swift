035b1459ed1da79142a52142cd88fe44e4a873dd
daukesnurmukhan <daukesnurmukhan@gmail.com>
2022-05-03 17:16:57 +0600

    Initial Commit

---
+++ Bookmark/BookmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookmarkApp.swift
+//  Bookmark
+//
+//  Created by Нұрмұхан Дәукес on 03.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BookmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
